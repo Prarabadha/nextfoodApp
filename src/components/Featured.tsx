0390ac2d6873c841a997243f6486e9df9071ec07
prarabdha bhagat <prarabadhabhagat@gmail.com>
2026-02-25 19:16:44 +0530

    second changes

--- src/components/Featured.tsx
+++ src/components/Featured.tsx
@@ -3,7 +3,7 @@ import Image from "next/image";
 
 const Featured = () => {
   return (
-    <div className="w-screen overflow-x-scroll text-red-500">
+    <div className="w-screen overflow-x-scroll text-red-400">
       {/* Wrapper */}
       <div className="w-max flex">
         {/* Single Item */}
